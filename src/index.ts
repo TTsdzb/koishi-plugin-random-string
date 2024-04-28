6b20dbc668aec3b9077aaf0e471773e50f89c13c
TTsdzb <ttsdzb@outlook.com>
2024-04-28 17:50:13 +0800

    feat: initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
 
-export const name = 'random-string'
+export const name = "random-string";
 
-export interface Config {}
+export interface Config {
+  chars: string;
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  chars: Schema.string().default(
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`-=[]\\;',./~!@#$%^&*()_+{}|:\"<>?"
+  ),
+}).i18n({
+  "zh-CN": require("./locales/zh-CN")._config,
+});
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, config: Config) {
+  // Register i18n
+  ctx.i18n.define("zh-CN", require("./locales/zh-CN"));
+
+  const pool = [];
+  for (const char of config.chars) pool.push(char);
+
+  ctx.command("rands <len:posint>").action(({ session }, len) => {
+    if (!len) return session.text(".lengthRequired");
+
+    let s = "";
+    while (s.length < len)
+      s += config.chars[Math.floor(Math.random() * config.chars.length)];
+
+    return s;
+  });
 }
